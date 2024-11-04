c7f4a03030a50864d960b385326920a6b3b8ee4c
bundulappp <bundulappp@gmail.com>
2024-11-04 10:49:49 +0100

    finish up to task 5

--- src/args.ts
+++ src/args.ts
@@ -1,3 +1,7 @@
-import { runArgsApp } from "./primary-adapters/args-input-primary-adapter";
-import { FakeStoreSecondaryAdapter } from "./secondary-adapters/fake-store-secondary-adapter";
+import { runArgsApp } from './primary-adapters/args-input-primary-adapter';
+import { FakeStoreSecondaryAdapter } from './secondary-adapters/fake-store-secondary-adapter';
 import { GreetingService } from './business/greeting-service';
+
+const storeAdapter = new FakeStoreSecondaryAdapter();
+const greetingService = new GreetingService(storeAdapter);
+runArgsApp(greetingService);
